5a78a1c8f6a2432e3e8f49fd866356ba786b19d0
Nethanel Kohen <nethanel.kohen@gmail.com>
2017-09-11 19:17:49 -0400

    final final

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ var people1 = require("./people1.json");
 var people2 = require("./people2.json");
 
 var merge = people1.concat(people2).sort();
-var string = "";
+merge.join("\n");
 
-for (var i = 0; i < merge.length; i++) {
-	string += merge[i] + "\n";
-}
-
-fs.writeFile("./sorted_people.txt", string, function(err) {
+fs.writeFile("./sorted_people.txt", merge, function(err) {
 	if (err) {
 		console.error("Error");
 		console.error(err);
